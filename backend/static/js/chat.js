65e26fdd641b884c4afed652fadcdd9132ba13ca
정소이 <010127js@gmail.com>
2023-11-18 14:03:52 +0900

    '수벙

--- backend/static/js/chat.js
+++ backend/static/js/chat.js
@@ -1,15 +1,13 @@
-
-
 // 채팅창
 // 메시지를 추가할때!!
-function addMessage(message, sender) {
+function addMessage(message, sender, id) {
   var chatBody = document.getElementById("chat-body");
   // 요소를 생성할때 div에 넣어서 만든다
   var messageContainer = document.createElement("div");
   var messageSender = document.createElement("div");
   var messageBubble = document.createElement("div");
   var messageTime = document.createElement("div");
-
+  
   var messageName = document.createElement("div");
   // var userImage = document.createElement('div');
   // var chatbotImage = document.createElement('div');
@@ -59,13 +57,13 @@ function addMessage(message, sender) {
 
     //TODO - 정연
     aaa = "user";
-    aImage.innerHTML = `<img src="/images/${aaa}.png" alt="${aaa}">`;
+    aImage.innerHTML = `<img src="/static/images/${aaa}.png" alt="${aaa}">`;
   } else {
     messageContainer.classList.add("chatbot-message");
 
     //TODO - 정연
     aaa = "chatbot";
-    aImage.innerHTML = `<img src="/images/${aaa}.png" alt="${aaa}">`;
+    aImage.innerHTML = `<img src="/static/images/${aaa}.png" alt="${aaa}">`;
   }
 
   // if (sender === 'user') {
@@ -94,10 +92,92 @@ function getTime() {
   return hours + ":" + minutes;
 }
 
-
-
-
 // 초기 대화 시작 -> jinja2로 이걸 띄워야 해
 setTimeout(function () {
   addMessage("안녕하세요! 어떤 도움이 필요하세요?", "DOBOT");
 }, 500);
+$("#toyou").click(function () {
+  // 입력된 데이터 가져오기
+
+  let inputData = $("#message-input").val(); // message-input의 값만 가져오기
+  if (inputData !== "") {
+    //  text 변수에 저장
+
+    let text = $(".message-container");
+
+    // text에 append해 요소 추가
+    addMessage(inputData, "user");
+    text.val("");
+    var chatBody = document.getElementById("chat-body");
+    chatBody.scrollTop = chatBody.scrollHeight;
+
+    // fastapi의 BaseModel은 JSON 형식을 받는데 그냥 data로 보내면 오류남
+    // contentType으로 JSON임을 알려주고 JSON.stringify로 데이터를 JSON으로 변환
+
+    $.ajax({
+      type: "post", // 어떤 방식으로 보낼지
+      url: "http://127.0.0.1:8000/dobot", // 보낼 주소
+      contentType: "application/json", // 서버에 JSON 형식임을 알려줌
+      data: JSON.stringify({ data: [inputData, getTime()] }), // 데이터를 JSON 문자열로 변환
+
+      // 성공적인 전송시 서버에서 반환된 response 받음
+      success: function (response) {
+        // 서버 응답을 성공하면 실행
+
+        addMessage(response.processed_data, "DOBOT");
+        
+        var chatBody = document.getElementById("chat-body");
+        chatBody.scrollTop = chatBody.scrollHeight;
+      },
+
+      error: function () {
+        alert("실패");
+      },
+    });
+
+    $("#message-input").val(""); // 입력 필드 새로고침
+  }
+});
+
+// 엔터시 작동
+$("#message-input").keyup(function (event) {
+  if (event.key === "Enter") {
+    // 입력된 데이터 가져오기
+
+   let inputData = $("#message-input").val(); // message-input의 값만 가져오기
+  if (inputData !== "") {
+    //  text 변수에 저장
+
+    let text = $(".message-container");
+
+    // text에 append해 요소 추가
+    addMessage(inputData, "user");
+    var chatBody = document.getElementById("chat-body");
+    chatBody.scrollTop = chatBody.scrollHeight;
+
+    // fastapi의 BaseModel은 JSON 형식을 받는데 그냥 data로 보내면 오류남
+    // contentType으로 JSON임을 알려주고 JSON.stringify로 데이터를 JSON으로 변환
+
+    $.ajax({
+      type: "post", // 어떤 방식으로 보낼지
+      url: "http://127.0.0.1:8000/dobot", // 보낼 주소
+      contentType: "application/json", // 서버에 JSON 형식임을 알려줌
+      data: JSON.stringify({ data: [inputData, getTime()] }), // 데이터를 JSON 문자열로 변환
+
+      // 성공적인 전송시 서버에서 반환된 response 받음
+      success: function (response) {
+        // 서버 응답을 성공하면 실행
+
+       addMessage(response.processed_data, "DOBOT");
+        var chatBody = document.getElementById("chat-body");
+        chatBody.scrollTop = chatBody.scrollHeight;
+      },
+
+      error: function () {
+        alert("실패");
+      },
+    });
+
+    $("#message-input").val(""); // 입력 필드 새로고침
+  }
+}});
---
+++ backend/static/js/login .js
@@ -0,0 +1,24 @@
+$("#toyou").click(function () {
+  // 입력된 데이터 가져오기
+
+  let inputData = $("#login").val(); // message-input의 값만 가져오기
+  let inputpassword = $("#password").val();
+  if (inputData !== "" && inputpassword !== "") {
+    $.ajax({
+      type: "post", // 어떤 방식으로 보낼지
+      url: "http://127.0.0.1:8000/login", // 보낼 주소
+      contentType: "application/json", // 서버에 JSON 형식임을 알려줌
+      data: JSON.stringify({ login: [inputData, inputpassword] }), // 데이터를 JSON 문자열로 변환
+      success: function (response) {
+        alert(response.message);
+        window.location.href = "/dobot";
+      },
+      error: function () {
+        alert("다시 시도해주세요");
+        $("#login").val("");
+        $("#password").val("");
+      },
+    });
+  }
+});
+                    
