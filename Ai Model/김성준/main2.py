b5897ead5b383ed915095b299a6a9fe526dbe933
김성준 <korea95124@gmail.com>
2023-11-23 09:54:43 +0900

    model+api commit

--- Ai Model/김성준/main2.py
+++ Ai Model/김성준/main2.py
@@ -1,29 +1,32 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import torch
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import GPT2LMHeadModel, PreTrainedTokenizerFast
 
+# FastAPI 앱 생성
 app = FastAPI()
 
-# 토크나이저와 모델 초기화
-tokenizer = BertTokenizer.from_pretrained('monologg/kobert')
-model = BertForSequenceClassification.from_pretrained('monologg/kobert', num_labels=4436, state_dict=torch.load('model_file_path.pth'))
-model.eval()
+# 요청 데이터 모델 정의
+class UserRequest(BaseModel):
+    question: str
 
-class Item(BaseModel):
-    sentence: str
+# 모델 및 토크나이저 로딩
+model = GPT2LMHeadModel.from_pretrained('C:\\Users\\gjaischool\\Documents\\GitHub\\D.O2\\Ai Model\\김성준\\saved_model')
+tokenizer = PreTrainedTokenizerFast.from_pretrained('C:\\Users\\gjaischool\\Documents\\GitHub\\D.O2\\Ai Model\\김성준\\saved_model')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
-@app.post("/predict")
-def predict(item: Item):
-    # 문장을 모델에 입력하기 위해 토크나이징
-    inputs = tokenizer(item.sentence, return_tensors="pt", padding=True, truncation=True, max_length=512)
-
-    # 모델로부터 예측 수행
+# 엔드포인트 정의
+@app.post("/chat")
+def chat(request: UserRequest):
+    model.eval()
+    input_ids = tokenizer.encode(request.question + tokenizer.eos_token, return_tensors="pt").to(device)
+    
+    # 모델이 응답 생성
     with torch.no_grad():
-        outputs = model(**inputs)
+        output = model.generate(input_ids, max_length=100)
+    
+    reply = tokenizer.decode(output[0], skip_special_tokens=True)
+    return {"reply": reply}
 
-    # 예측 결과 추출
-    probabilities = torch.nn.functional.softmax(outputs.logits, dim=1)
-    prediction = probabilities.argmax().item()
 
-    return {"prediction": prediction}
