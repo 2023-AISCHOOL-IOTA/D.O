7c3b35b7c0ae47c948556af135d7d968f7b7fdf0
윤혜민 <minim2948@naver.com>
2023-11-27 08:36:23 +0900

    11.26 서비스 정리

--- web/templete/js/game.js
+++ web/templete/js/game.js
@@ -1,156 +1,370 @@
-(function($) {
-    $.fn.extend({
-  
-      roulette: function(options) {
-  
-        var defaults = {
-          angle: 0,
-          angleOffset: -45,
-          speed: 5000,
-          easing: "easeInOutElastic",
-        };
-  
-        var opt = $.extend(defaults, options);
-  
-        return this.each(function() {
-          var o = opt;
-  
-          var data = [
-                      {
-              color: '#3f297e',
-              text: 'N분의 1'
-            },
-            {
-              color: '#1d61ac',
-              text: '요즘것들'
-            },
-            {
-              color: '#169ed8',
-              text: '도박'
-            },
-            {
-              color: '#209b6c',
-              text: '젓가락'
-            },
-            {
-              color: '#60b236',
-              text: '거북선'
-            },
-            {
-              color: '#efe61f',
-              text: '겁'
-            },
-            {
-              color: '#f7a416',
-              text: 'Day Day'
-            },
-            {
-              color: '#e6471d',
-              text: '호랑나비'
-            },
-            {
-              color: '#dc0936',
-              text: 'Okey Dokey'
-            },
-            {
-              color: '#e5177b',
-              text: '오빠차'
-            },
-            {
-              color: '#be107f',
-              text: 'RESPECT'
-            },
-            {
-              color: '#881f7e',
-              text: '작두'
-            }
-          ];
-  
-          var $wrap = $(this);
-          var $btnStart = $wrap.find("#btn-start");
-          var $roulette = $wrap.find(".roulette");
-          var wrapW = $wrap.width();
-          var angle = o.angle;
-          var angleOffset = o.angleOffset;
-          var speed = o.speed;
-          var esing = o.easing;
-          var itemSize = data.length;
-          var itemSelector = "item";
-          var labelSelector = "label";
-          var d = 360 / itemSize;
-          var borderTopWidth = wrapW;
-          var borderRightWidth = tanDeg(d);
-  
-          for (i = 1; i <= itemSize; i += 1) {
-            var idx = i - 1;
-            var rt = i * d + angleOffset;
-            var itemHTML = $('<div class="' + itemSelector + '">');
-            var labelHTML = '';
-                labelHTML += '<p class="' + labelSelector + '">';
-                labelHTML += '	<span class="text">' + data[idx].text + '<\/span>';
-                labelHTML += '<\/p>';
-  
-            $roulette.append(itemHTML);
-            $roulette.children("." + itemSelector).eq(idx).append(labelHTML);
-            $roulette.children("." + itemSelector).eq(idx).css({
-              "left": wrapW / 2,
-              "top": -wrapW / 2,
-              "border-top-width": borderTopWidth,
-              "border-right-width": borderRightWidth,
-              "border-top-color": data[idx].color,
-              "transform": "rotate(" + rt + "deg)"
-            });
-  
-            var textH = parseInt(((2 * Math.PI * wrapW) / d) * .5);
-  
-            $roulette.children("." + itemSelector).eq(idx).children("." + labelSelector).css({
-              "height": textH + 'px',
-              "line-height": textH + 'px',
-              "transform": 'translateX(' + (textH * 1.3) + 'px) translateY(' + (wrapW * -.3) + 'px) rotateZ(' + (90 + d * .5) + 'deg)'
-            });
-  
-          }
-  
-          function tanDeg(deg) {
-            var rad = deg * Math.PI / 180;
-            return wrapW / (1 / Math.tan(rad));
-          }
-  
-  
-          $btnStart.on("click", function() {
-            rotation();
-          });
-  
-          function rotation() {
-  
-            var completeA = 360 * r(5, 10) + r(0, 360);
-  
-            $roulette.rotate({
-              angle: angle,
-              animateTo: completeA,
-              center: ["50%", "50%"],
-              easing: $.easing.esing,
-              callback: function() {
-                var currentA = $(this).getRotateAngle();
-  
-                console.log(currentA);
-  
-              },
-              duration: speed
-            });
-          }
-  
-          function r(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-          }
-  
-        });
+$(function(){
+
+  var heightNode = 10;
+  var widthNode =  0;
+
+  var LADDER = {};
+  var row =0;
+  var ladder = $('#ladder');
+  var ladder_canvas = $('#ladder_canvas');
+  var GLOBAL_FOOT_PRINT= {};
+  var GLOBAL_CHECK_FOOT_PRINT= {};
+  var working = false;
+  function init(){
+      canvasDraw();
+  }
+
+  $('#button').on('click', function(){
+      var member = $('input[name=member]').val();
+      if(member < 2){
+          return alert('최소 2명 이상 선택하세요.')
       }
-    });
-  })(jQuery);
-  
-  $(function() {
-  
-    $('.box-roulette').roulette();
-  
+
+      if(member > 20){
+          return alert('너무 많아요.. ㅠㅠ')   
+      }
+      $('#landing').css({
+          'opacity': 0
+      });
+      widthNode = member;
+      setTimeout(function(){
+          $('#landing').remove();
+          init();
+      }, 310)
+
   });
+
+  function canvasDraw(){
+      ladder.css({
+          'width' :( widthNode-1) * 100 + 6,
+          'height' : (heightNode -1 ) * 25 + 6,
+          'background-color' : '#fff'
+      });
+     ladder_canvas
+     .attr('width' , ( widthNode-1) * 100 + 6)
+     .attr('height' , ( heightNode-1) * 25 + 6);
+
+      setDefaultFootPrint();
+      reSetCheckFootPrint();
+      setDefaultRowLine();
+      setRandomNodeData();
+      drawDefaultLine();
+      drawNodeLine();
+      userSetting();
+      resultSetting();
+      
+  }
+  var userName = "";
+  $(document).on('click', 'button.ladder-start', function(e){
+      if(working){
+          return false;
+      }
+      $('.dim').remove();
+      working = true;
+      reSetCheckFootPrint();
+      var _this = $(e.target);
+      _this.attr('disabled' ,  true).css({
+          'color' : '#000',
+          'border' : '1px solid #F2F2F2',
+          'opacity' : '0.3'
+      })
+      var node = _this.attr('data-node');
+      var color =  _this.attr('data-color');
+      startLineDrawing(node, color);
+      userName =  $('input[data-node="'+node+'"]').val();
+  })
+  
+  function startLineDrawing(node , color){
+
+      var node = node;
+      var color = color;
+      
+      var x = node.split('-')[0]*1;
+      var y = node.split('-')[1]*1;
+      var nodeInfo = GLOBAL_FOOT_PRINT[node];
+
+      GLOBAL_CHECK_FOOT_PRINT[node] = true;
+      
+      var dir = 'r'
+      if(y ==heightNode ){
+          reSetCheckFootPrint();
+          var target = $('input[data-node="'+node+'"]');
+          target.css({
+              'background-color' : color
+          })
+          $('#' + node + "-user").text(userName)
+           working = false;
+          return false;
+      }
+      if(nodeInfo["change"] ){
+          var leftNode = (x-1) + "-" +y;
+          var rightNode = (x+1) + "-" +y;
+          var downNode = x +"-"+ (y + 1);
+          var leftNodeInfo = GLOBAL_FOOT_PRINT[leftNode];
+          var rightNodeInfo = GLOBAL_FOOT_PRINT[rightNode];
+              
+          if(GLOBAL_FOOT_PRINT.hasOwnProperty(leftNode) && GLOBAL_FOOT_PRINT.hasOwnProperty(rightNode)){      
+              var leftNodeInfo = GLOBAL_FOOT_PRINT[leftNode];
+              var rightNodeInfo = GLOBAL_FOOT_PRINT[rightNode];
+              if(  (leftNodeInfo["change"] &&  leftNodeInfo["draw"] && !!!GLOBAL_CHECK_FOOT_PRINT[leftNode] ) && (rightNodeInfo["change"])&&  leftNodeInfo["draw"]  && !!!GLOBAL_CHECK_FOOT_PRINT[rightNode] ){
+                  //Left우선 
+                  console.log("중복일때  LEFT 우선");
+                  stokeLine(x, y, 'w' , 'l' , color ,3)
+                   setTimeout(function(){ 
+                       return startLineDrawing(leftNode, color)
+                   }, 100);
+              }
+              else if(  (leftNodeInfo["change"] &&  !!!leftNodeInfo["draw"] && !!!GLOBAL_CHECK_FOOT_PRINT[leftNode] ) && (rightNodeInfo["change"]) && !!!GLOBAL_CHECK_FOOT_PRINT[rightNode] ){
+                  console.log('RIGHT 우선')
+                  stokeLine(x, y, 'w' , 'r' , color ,3)
+                  console.log("right")
+                  setTimeout(function(){ 
+                      return startLineDrawing(rightNode, color)
+                   }, 100);
+              }
+              else if(  (leftNodeInfo["change"] &&  leftNodeInfo["draw"] && !!!GLOBAL_CHECK_FOOT_PRINT[leftNode] ) && (!!!rightNodeInfo["change"]) ){
+                  //Left우선 
+                  console.log("LEFT 우선");
+                  stokeLine(x, y, 'w' , 'l' , color ,3)
+                   setTimeout(function(){ 
+                       return startLineDrawing(leftNode, color)
+                   }, 100);
+              }
+               else if(  !!!leftNodeInfo["change"]  &&  (rightNodeInfo["change"]) && !!!GLOBAL_CHECK_FOOT_PRINT[rightNode] ){
+                  //Right우선 
+                  console.log("RIGHT 우선");
+                  stokeLine(x, y, 'w' , 'r' , color ,3)
+                   setTimeout(function(){ 
+                       return startLineDrawing(rightNode, color)
+                   }, 100);
+              }
+              else{
+                  console.log('DOWN 우선')
+                  stokeLine(x, y, 'h' , 'd' , color ,3)
+                  setTimeout(function(){ 
+                     return startLineDrawing(downNode, color)
+                  }, 100);
+              }
+          }else{
+              console.log('else')
+             if(!!!GLOBAL_FOOT_PRINT.hasOwnProperty(leftNode) && GLOBAL_FOOT_PRINT.hasOwnProperty(rightNode)){      
+                  /// 좌측라인
+                  console.log('좌측라인')
+                  if(  (rightNodeInfo["change"] && !!!rightNodeInfo["draw"] ) && !!!GLOBAL_CHECK_FOOT_PRINT[rightNode] ){
+                      //Right우선 
+                      console.log("RIGHT 우선");
+                      stokeLine(x, y, 'w' , 'r' , color ,3)
+                      setTimeout(function(){ 
+                          return startLineDrawing(rightNode, color)
+                      }, 100);
+                  }else{
+                      console.log('DOWN')
+                      stokeLine(x, y, 'h' , 'd' , color ,3)
+                      setTimeout(function(){ 
+                         return startLineDrawing(downNode, color)
+                      }, 100);
+                  }
+                  
+             }else if(GLOBAL_FOOT_PRINT.hasOwnProperty(leftNode) && !!!GLOBAL_FOOT_PRINT.hasOwnProperty(rightNode)){      
+                  /// 우측라인
+                  console.log('우측라인')
+                  if(  (leftNodeInfo["change"] && leftNodeInfo["draw"] ) && !!!GLOBAL_CHECK_FOOT_PRINT[leftNode] ){
+                      //Right우선 
+                      console.log("LEFT 우선");
+                      stokeLine(x, y, 'w' , 'l' , color ,3)
+                      setTimeout(function(){ 
+                          return startLineDrawing(leftNode, color)
+                      }, 100);
+                  }else{
+                      console.log('DOWN')
+                      stokeLine(x, y, 'h' , 'd' , color ,3)
+                      setTimeout(function(){ 
+                         return startLineDrawing(downNode, color)
+                      }, 100);
+                  }
+             }
+          }
+
+
+      }else{
+          console.log("down")
+          var downNode = x +"-"+ (y + 1);
+          stokeLine(x, y, 'h' , 'd' , color ,3)
+          setTimeout(function(){ 
+              return startLineDrawing(downNode, color)
+           }, 100);
+      }
+  }
+
+
+
+  function userSetting(){
+      var userList = LADDER[0];
+      var html = '';
+      for(var i=0; i <  userList.length; i++){
+          var color = '#'+(function lol(m,s,c){return s[m.floor(m.random() * s.length)] + (c && lol(m,s,c-1));})(Math,'0123456789ABCDEF',4);
+
+          var x = userList[i].split('-')[0]*1;
+          var y = userList[i].split('-')[1]*1;
+          var left = x * 100  -30
+          html += '<div class="user-wrap" style="left:'+left+'"><input type="text" data-node="'+userList[i]+'"><button class="ladder-start" style="background-color:'+color+'" data-color="'+color+'" data-node="'+userList[i]+'"></button>';
+          html +='</div>'
+      }
+      ladder.append(html);
+  }
+  function resultSetting(){
+       var resultList = LADDER[heightNode-1];
+       console.log(resultList )
+
+      var html = '';
+      for(var i=0; i <  resultList.length; i++){
+          
+          var x = resultList[i].split('-')[0]*1;
+          var y = resultList[i].split('-')[1]*1 + 1;
+          var node = x + "-" + y;
+          var left = x * 100  -30
+          html += '<div class="answer-wrap" style="left:'+left+'"><input type="text" data-node="'+node+'">';
+          html +='<p id="'+node+'-user"></p>'
+          html +='</div>'
+      }
+      ladder.append(html);
+  }
+
+  function drawNodeLine(){
+
+      for(var y =0; y < heightNode; y++){
+          for(var x =0; x <widthNode ; x++){
+              var node = x + '-' + y;
+              var nodeInfo  = GLOBAL_FOOT_PRINT[node];
+              if(nodeInfo["change"] && nodeInfo["draw"] ){
+                   stokeLine(x, y ,'w' , 'r' , '#ddd' , '2')
+              }else{
+
+              }
+          }
+      }
+  }
+
+  function stokeLine(x, y, flag , dir , color , width){
+      var canvas = document.getElementById('ladder_canvas');
+      var ctx = canvas.getContext('2d');
+      var moveToStart =0, moveToEnd =0, lineToStart =0 ,lineToEnd =0; 
+      var eachWidth = 100; 
+      var eachHeight = 25;
+      if(flag == "w"){
+          //가로줄
+         
+         
+          if(dir == "r"){
+              ctx.beginPath();
+              moveToStart = x * eachWidth ;
+              moveToEnd = y * eachHeight ;
+              lineToStart = (x+ 1) * eachWidth;
+              lineToEnd = y * eachHeight;
+              
+          }else{
+              // dir "l"
+               ctx.beginPath();
+              moveToStart = x * eachWidth;
+              moveToEnd = y * eachHeight;
+              lineToStart = (x- 1) * eachWidth;
+              lineToEnd = y * eachHeight;
+          }
+      }else{
+              ctx.beginPath();
+              moveToStart = x * eachWidth ;
+              moveToEnd = y * eachHeight;
+              lineToStart = x * eachWidth ;
+              lineToEnd = (y+1) * eachHeight;
+      }
+
+      ctx.moveTo(moveToStart + 3 ,moveToEnd  + 2);
+      ctx.lineTo(lineToStart  + 3 ,lineToEnd  + 2 );
+      ctx.strokeStyle = color;
+      ctx.lineWidth = width;
+      ctx.stroke();
+      ctx.closePath();
+  }
+
+  function drawDefaultLine(){
+      var html = '';
+      html += '<table>'
+       for(var y =0; y < heightNode-1; y++){
+          html += '<tr>';
+          for(var x =0; x <widthNode-1 ; x++){
+              html += '<td style="width:98px; height:25px; border-left:2px solid #ddd; border-right:2px solid #ddd;"></td>';
+          }
+          html += '</tr>';
+      }
+      html += '</table>'
+      ladder.append(html);
+  }
+
+  function setRandomNodeData(){
+       for(var y =0; y < heightNode; y++){
+          for(var x =0; x <widthNode ; x++){
+              var loopNode = x + "-" + y;
+              var rand = Math.floor(Math.random() * 2);
+              if(rand == 0){
+                  GLOBAL_FOOT_PRINT[loopNode] = {"change" : false , "draw" : false}
+              }else{
+                  if(x == (widthNode - 1)){
+                      GLOBAL_FOOT_PRINT[loopNode] = {"change" : false , "draw" : false} ;    
+                  }else{
+                      GLOBAL_FOOT_PRINT[loopNode] =  {"change" : true , "draw" : true} ;  ;
+                      x = x + 1;
+                       loopNode = x + "-" + y;
+                       GLOBAL_FOOT_PRINT[loopNode] =  {"change" : true , "draw" : false} ;  ;
+                  }
+              }
+          }
+       }
+  }
+
+  function setDefaultFootPrint(){
+    
+      for(var r = 0; r < heightNode; r++){
+          for(var column =0; column < widthNode; column++){
+              GLOBAL_FOOT_PRINT[column + "-" + r] = false;
+          }
+      }
+  }
+  function reSetCheckFootPrint(){
+
+      for(var r = 0; r < heightNode; r++){
+          for(var column =0; column < widthNode; column++){
+              GLOBAL_CHECK_FOOT_PRINT[column + "-" + r] = false;
+          }
+      }
+  }
+
+  function setDefaultRowLine(){
+
+      for(var y =0; y < heightNode; y++){
+          var rowArr = [];
+          for(var x =0; x <widthNode ; x++){
+              var node = x + "-"+ row;
+              rowArr .push(node);
+              // 노드그리기
+              var left = x * 100;
+              var top = row * 25;
+              var node = $('<div></div>')
+              .attr('class' ,'node')
+              .attr('id' , node)
+              .attr('data-left' , left)
+              .attr('data-top' , top)
+              .css({
+                  'position' : 'absolute',
+                  'left' : left,
+                  'top' : top
+              });
+              ladder.append(node);
+           }
+           LADDER[row] =  rowArr;
+           row++;
+      }
+  }
+
+
+
+});
